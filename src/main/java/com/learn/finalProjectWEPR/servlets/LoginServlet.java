db4567c24e5b9e1f107cfa826bf5fa673e1278d6
HOANGHIEU003 <hoanghieu031299@gmail.com>
2021-01-14 19:51:07 +0700

    #10. Authenticating login detail with proper message

---
+++ src/main/java/com/learn/finalProjectWEPR/servlets/LoginServlet.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.learn.finalProjectWEPR.servlets;
+
+import com.learn.finalProjectWEPR.dao.UserDao;
+import com.learn.finalProjectWEPR.entities.User;
+import com.learn.finalProjectWEPR.helper.FactoryProvider;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author HoangHieu
+ */
+public class LoginServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+
+            String email = request.getParameter("email");
+            String password = request.getParameter("password");
+
+            // validation
+            // authenticating user
+            UserDao userDao = new UserDao(FactoryProvider.getFactory());
+            User user = userDao.getUserByEmailAndPassword(email, password);
+//            System.out.println(user);
+            HttpSession httpSession = request.getSession();
+            if (user == null) {
+                httpSession.setAttribute("message", "InvalidDetails!! Try with another one");
+                response.sendRedirect("login.jsp");
+                return;
+            } else {
+                out.println("<h1>Welcome " + user.getUserName() + " </h1>");
+            }
+        }
+    }
+        // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+        /**
+         * Handles the HTTP <code>GET</code> method.
+         *
+         * @param request servlet request
+         * @param response servlet response
+         * @throws ServletException if a servlet-specific error occurs
+         * @throws IOException if an I/O error occurs
+         */
+        @Override
+        protected void doGet
+        (HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            processRequest(request, response);
+        }
+
+        /**
+         * Handles the HTTP <code>POST</code> method.
+         *
+         * @param request servlet request
+         * @param response servlet response
+         * @throws ServletException if a servlet-specific error occurs
+         * @throws IOException if an I/O error occurs
+         */
+        @Override
+        protected void doPost
+        (HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            processRequest(request, response);
+        }
+
+        /**
+         * Returns a short description of the servlet.
+         *
+         * @return a String containing servlet description
+         */
+        @Override
+        public String getServletInfo
+        
+            () {
+        return "Short description";
+        }// </editor-fold>
+
+    }
--- target/finalProjectWEPR-1.0-SNAPSHOT/login.jsp
+++ target/finalProjectWEPR-1.0-SNAPSHOT/login.jsp
@@ -23,15 +23,16 @@
                         </div>
 
                         <div class="card-body">
-                            <form action="LogínServlet" method="post">
+                            <%@include file="components/message.jsp" %>
+                            <form action="LoginServlet" method="post">
                                 <div class="form-group">
                                     <label for="exampleInputEmail1">Email address</label>
-                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                                    <input name = "email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
                                     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                                 </div>
                                 <div class="form-group">
                                     <label for="exampleInputPassword1">Password</label>
-                                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                                    <input name = "password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                                 </div>
                                 <a href="register.jsp">If not register click here</a>
                                 <div class="container text-center">
